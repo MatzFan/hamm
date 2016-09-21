9e559a51f6395c46f395e8bd877ebc539869cfd7
Stefan Pochmann <Stefan Pochmann>
2016-09-21 06:01:25 +0200

    Added test.bat for showing how I compiled and ran.

---
+++ test.bat
@@ -0,0 +1,16 @@
+gcc -O3 -Wall -Wextra -Werror -std=gnu99 tree.c -o tree
+
+tree bk     1000 100000000 1 1 2 3 4 5 6 7 8 9 10 > test_bk1.txt
+tree vp     1000 100000000 1 1 2 3 4 5 6 7 8 9 10 > test_vp1.txt
+tree linear 1000 100000000 1 1 2 3 4 5 6 7 8 9 10 > test_linear1.txt
+tree bitset 1000 100000000 1 1 2 3 4 5 6 7 8 9 10 > test_bitset1.txt
+
+tree bk     1000 100000000 10 1 2 3 4 5 6 7 8 9 10 > test_bk2.txt
+tree vp     1000 100000000 10 1 2 3 4 5 6 7 8 9 10 > test_vp2.txt
+tree linear 1000 100000000 10 1 2 3 4 5 6 7 8 9 10 > test_linear2.txt
+tree bitset 1000 100000000 10 1 2 3 4 5 6 7 8 9 10 > test_bitset2.txt
+
+tree bk     1000 100000000 10 1 2 3 4 5 6 7 8 9 10 > test_bk3.txt
+tree vp     1000 100000000 10 1 2 3 4 5 6 7 8 9 10 > test_vp3.txt
+tree linear 1000 100000000 10 1 2 3 4 5 6 7 8 9 10 > test_linear3.txt
+tree bitset 1000 100000000 10 1 2 3 4 5 6 7 8 9 10 > test_bitset3.txt
